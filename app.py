b01dfab25b558ded552ae693809270ccc949803f
HyuugaKojiro1612 <minhquannguyenhue@gmail.com>
2024-02-22 22:00:12 +0700

    Generate multiple outputs with SequentialChain

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from langchain_community.llms import openai
 from langchain_openai import ChatOpenAI
 from langchain_openai import OpenAI
 from langchain.prompts import PromptTemplate, ChatPromptTemplate
-from langchain.chains import LLMChain
+from langchain.chains import LLMChain, SequentialChain
 
 
 from dotenv import load_dotenv, find_dotenv
@@ -13,21 +13,29 @@ _ = load_dotenv(find_dotenv()) # read local .env file
 
 # App framework
 st.title('🦜🔗 Youtube GPT Creator')
-prompt = st.text_input('Plug in your prompt here')
+prompt = st.text_input('Plug in your topic here')
 
-# 
-template_string = '''Write a Youtube video title about {topic}.
-'''
-title_template = PromptTemplate.from_template(template_string)
+# Prompt templates
 # title_template = ChatPromptTemplate.from_template(template_string)
+title_prompt = PromptTemplate.from_template('Write a Youtube video title about {topic}.')
+script_prompt = PromptTemplate.from_template('Write a Youtube video script based on this title.\nTITLE: {title}')
 
 # LLM
 # llm = OpenAI(temperature=0.9)
 llm = ChatOpenAI(temperature=0.9, model="gpt-3.5-turbo-0125")
-title_chain = LLMChain(llm=llm, prompt=title_template, verbose=True)
+title_chain = LLMChain(llm=llm, prompt=title_prompt, output_key='title', verbose=True)
+script_chain = LLMChain(llm=llm, prompt=script_prompt, output_key='script', verbose=True)
+
+sequential_chain = SequentialChain(
+    chains=[title_chain, script_chain], 
+    input_variables= ['topic'], 
+    output_variables= ['title', 'script'],
+    verbose=True
+)
 
 
 # Output rendering
 if prompt:
-    response = title_chain.run(prompt)
-    st.write(response)
+    response = sequential_chain({'topic': prompt})
+    st.write(response['title'])
+    st.write(response['script'])
